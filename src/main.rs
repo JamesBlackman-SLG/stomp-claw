03334801f107740e7fa0c4d1fc4157697c2f3c63
James Blackman <james@silverlininggroup.co.uk>
2026-03-07 14:42:03 +0000

    fix: add meta cache-control headers to prevent stale JS

--- src/main.rs
+++ src/main.rs
@@ -95,6 +95,9 @@ const HELP_HTML: &str = r##"<div class="help-page">
 const VIEWER_HTML: &str = r#"<!DOCTYPE html>
 <html>
 <head>
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Expires" content="0">
     <meta charset="utf-8">
     <title>Stomp Claw Live</title>
     <style>
